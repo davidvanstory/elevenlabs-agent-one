23ca18b2313d27abe8dfa9292e0bcde94745d6d6
Dylan Holland <dylaneholland@gmail.com>
2025-02-19 09:48:55 -0800

    basic-elevenlabs-agent: Adding some more routes to test

--- agent/__main__.py
+++ agent/__main__.py
@@ -6,7 +6,13 @@ app = FastAPI()
 def read_root() -> dict[str, str]:
     return {}
 
-@app.post("/agent/accept")
+@app.post("/agent/take-note")
+async def accept_agent(request: Request) -> dict[str, str]:
+    request_body = await request.json()
+    print(request_body)
+    return {}
+
+@app.post("/agent/initialize")
 async def accept_agent(request: Request) -> dict[str, str]:
     request_body = await request.json()
     print(request_body)
